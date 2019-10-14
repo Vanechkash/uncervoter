6494db01885fd9d5bb13f4bf27e933d16f305f5c
Ваня <vanechkashevtsov@gmail.com>
2019-10-14 21:14:28 +0300

    1st part commit

---
+++ uncervoter/polls/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, You're at the Polls index")
+
+
+# Create your views here.
